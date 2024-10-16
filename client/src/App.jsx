7ca157451e8b62dda6de6852fba28efad52f8f05
Durgesh Katyayan <youngkong9601@gmail.com>
2024-10-16 11:07:56 +0530

    routes in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import Login from './components/signIn/Login.jsx'
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Login/>}/>
+      </Routes>
+    </Router>
   )
 }
 
--- client/src/components/signIn/Login.jsx
+++ client/src/components/signIn/Login.jsx
@@ -1,1 +1,9 @@
-r
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
