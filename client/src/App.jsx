17e8199378ec41974a8f3cf10ea5cf95a964ddfe
Durgesh <youngkong9601@gmail.com>
2024-10-16 12:05:59 +0530

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@
 
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import Login from './components/signIn/Login.jsx'
+import Dashboard from './components/Dashboard.jsx'
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' exact element={<Login/>}/>
+        {/* <Route path='/' exact element={<Dashboard/>}/> */}
       </Routes>
     </Router>
 
--- client/src/components/signIn/Login.jsx
+++ client/src/components/signIn/Login.jsx
@@ -2,8 +2,8 @@
 import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 // import './index.css';
-import BackgroundImage from '../../assets/wallpaperflare.com_wallpaper4.jpg';
-import Logo from '../../assets/logo.png';
+import BackgroundImage from '../../assets/wallpaperflare.com_wallpaper.jpg';
+import Logo from '../../assets/logo1.png';
 
 
 
@@ -11,10 +11,10 @@ const Login = () => {
   const [loginData, setLoginData] = useState({
     username: '',
     password: '',
-    remember: false, // Set default to false
+    remember: false,
   });
 
-  const [confirmPassword, setConfirmPassword] = useState(''); // Separate state for confirmation
+  const [confirmPassword, setConfirmPassword] = useState(''); 
   const [popUp, setPopup] = useState(true);
 
   const handleSubmit = (e) => {
@@ -22,12 +22,12 @@ const Login = () => {
     console.log(loginData);
 
     if (!popUp) {
-      // Handle signup here if popUp is false
+      
       if (loginData.password !== confirmPassword) {
         alert("Passwords do not match!");
         return;
       }
-      // You may want to add more signup logic here
+      
     }
   };
 
@@ -36,18 +36,21 @@ const Login = () => {
     setLoginData((prevData) => ({ ...prevData, [name]: name === 'remember' ? checked : value }));
 
     if (name === 'confirmPassword') {
-      setConfirmPassword(value); // Update confirmation password
+      setConfirmPassword(value); 
     }
   };
 
   return (
     <>
-     {/* <img src={Logo} alt="Logo" style={{ width: '100%', height: 'auto' }} /> */}
     <div
-      className="min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat relative"
+      className="min-h-screen flex flex-col items-center  bg-cover bg-center bg-no-repeat relative "
       style={{ backgroundImage: `url(${BackgroundImage})` }}
     >
-
+      {/* Logo at the top */}
+      <div className="mb-6 backdrop-blur-lg backdrop-opacity-65 mx-16 ">
+        <img src={Logo} alt="Logo" style={{ width: '75%', height: 'auto' }} />
+      </div>
+  
       {popUp ? (
         <div className="w-full max-w-md p-8 rounded-lg border border-white/50 bg-white/10 backdrop-blur-md">
           <form onSubmit={handleSubmit} className="flex flex-col">
@@ -58,20 +61,20 @@ const Login = () => {
                 name="username"
                 required
                 value={loginData.username}
-                placeholder='Enter your username'
+                placeholder="Enter your username"
                 onChange={handleChange}
-                className="w-full bg-transparent border-none outline-none text-white py-2 focus:outline-none placeholder-white"
+                className="w-full bg-transparent border-none outline-none text-white py-2 focus:outline-none placeholder-gray-300"
               />
             </div>
             <div className="relative border-b-2 border-gray-300 mb-6">
               <input
                 type="password"
-                name='password'
+                name="password"
                 required
                 value={loginData.password}
                 onChange={handleChange}
-                placeholder='Enter your password'
-                className="w-full bg-transparent border-none outline-none text-white py-2 focus:outline-none placeholder-white"
+                placeholder="Enter your password"
+                className="w-full bg-transparent border-none outline-none text-white py-2 focus:outline-none placeholder-gray-300"
               />
             </div>
             <div className="flex justify-between items-center text-white mb-8">
@@ -82,19 +85,22 @@ const Login = () => {
                   name="remember"
                   checked={loginData.remember}
                   onChange={handleChange}
-                  className="hidden" // Hide the default checkbox
+                  className="hidden"
                 />
                 <span className="w-12 h-6 flex items-center bg-gray-400 rounded-full p-1 duration-300 ease-in-out">
                   <span
-                    className={`bg-white w-4 h-4 rounded-full shadow-md transform duration-300 ease-in-out ${loginData.remember ? "translate-x-6 bg-blue-500" : "translate-x-0"
-                      }`}
+                    className={`bg-white w-4 h-4 rounded-full shadow-md transform duration-300 ease-in-out ${
+                      loginData.remember ? 'translate-x-6 bg-blue-500' : 'translate-x-0'
+                    }`}
                   />
                 </span>
                 <p className="ml-2">Remember me</p>
               </label>
-              <a onClick={() => setPopup(false)} href="#" className="text-gray-300 hover:underline">Forgot password?</a>
+              <a onClick={() => setPopup(false)} href="#" className="text-gray-300 hover:underline">
+                Forgot password?
+              </a>
             </div>
-
+  
             <button
               type="submit"
               className="bg-white text-black font-semibold py-2 px-4 rounded-md border-2 border-transparent hover:bg-white/15 hover:text-white hover:border-white transition duration-300"
@@ -102,7 +108,12 @@ const Login = () => {
               Log In
             </button>
             <div className="text-center mt-6 text-white">
-              <p>Don't have an account? <a onClick={() => setPopup(false)} className="text-blue-600 hover:underline ">Register</a></p>
+              <p>
+                Don't have an account?{' '}
+                <a onClick={() => setPopup(false)} className="text-blue-600 hover:underline">
+                  Register
+                </a>
+              </p>
             </div>
           </form>
         </div>
@@ -116,7 +127,7 @@ const Login = () => {
                 name="username"
                 required
                 value={loginData.username}
-                placeholder='Enter Username'
+                placeholder="Enter Username"
                 onChange={handleChange}
                 className="w-full bg-transparent border-none outline-none text-white py-2 focus:outline-none placeholder-white"
               />
@@ -124,22 +135,22 @@ const Login = () => {
             <div className="relative border-b-2 border-gray-300 mb-6">
               <input
                 type="password"
-                name='password'
+                name="password"
                 required
                 value={loginData.password}
                 onChange={handleChange}
-                placeholder='Enter password'
+                placeholder="Enter password"
                 className="w-full bg-transparent border-none outline-none text-white py-2 focus:outline-none placeholder-white"
               />
             </div>
             <div className="relative border-b-2 border-gray-300 mb-6">
               <input
                 type="password"
-                name='confirmPassword' // Change this to confirmPassword
+                name="confirmPassword"
                 required
-                value={confirmPassword} // Use confirmPassword state
+                value={confirmPassword}
                 onChange={handleChange}
-                placeholder='confirm password'
+                placeholder="confirm password"
                 className="w-full bg-transparent border-none outline-none text-white py-2 focus:outline-none placeholder-white"
               />
             </div>
@@ -150,13 +161,19 @@ const Login = () => {
               SignUp
             </button>
             <div className="text-center mt-6 text-white">
-              <p>Already have an account? <a onClick={() => setPopup(true)} className="text-blue-600 hover:underline">Login</a></p>
+              <p>
+                Already have an account?{' '}
+                <a onClick={() => setPopup(true)} className="text-blue-600 hover:underline">
+                  Login
+                </a>
+              </p>
             </div>
           </form>
         </div>
       )}
     </div>
   </>
+  
   );
 };
 
