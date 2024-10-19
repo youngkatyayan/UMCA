bd3517b0423e61c2bf6493a67867f7a59fafe1b7
Durgesh Katyayan <youngkong9601@gmail.com>
2024-10-19 12:40:44 +0530

    frontDesign home

---
+++ client/src/components/user/Home.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import dove from '../../assets/gif/dove.gif'
+import phone from '../../assets/gif/phone.gif'
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoYoutube } from "react-icons/io";
+import { FaFacebook } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+const Home = () => {
+    return (
+        <div className=''>
+
+            <div className='bg-[hsl(189,54%,97%)] px-5 border w-full shadow-md flex items-center gap-2 sm:gap-5 justify-center flex-wrap md:flex-nowrap'>
+
+                <div className='flex gap-2 me-2'>
+                    <img src={dove} alt="" height={24} width={24} className='mix-blend-multiply' />
+                    <span className='text-[.9rem]'>useradmin@gmail.com</span>
+                </div>
+
+                <div className='clip hidden sm:flex'>clip first </div>
+
+                <div className='flex gap-2 whitespace-nowrap'>
+                    <img src={phone} alt="" height={24} width={24} className='mix-blend-multiply' />
+                    <span className='text-[.9rem]'>+91 9876543212</span>
+                </div>
+
+                <div className='clip1 hidden sm:flex'>clip first </div>
+
+                <div className='flex gap-4'>
+                    <Link className='h-full' to={''}><FaInstagramSquare className='h-6 rounded-full text-[#C13584]' /></Link>
+                    <Link className='h-full' to={''}><FaXTwitter className='h-6 rounded-full text-[#3498db]' /></Link>
+                    <Link className='h-full' to={''}><IoLogoYoutube className='h-6 rounded-full text-[#FF0000]' /></Link>
+                    <Link className='h-full' to={''}><FaFacebook className='h-6 rounded-full text-[#1877F2]' /></Link>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './components/store/Store.js'
