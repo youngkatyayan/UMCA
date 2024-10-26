d60331af4d7c7d851a35305eca48df02bed037c2
Durgesh <youngkong9601@gmail.com>
2024-10-26 13:27:59 +0530

    ok

--- server/routes/AdminRoutes/MastRoutes.js
+++ server/routes/AdminRoutes/MastRoutes.js
@@ -9,7 +9,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const uploadDir = path.join(__dirname, 'courseimage', 'uploads');
+const uploadDir = path.join(__dirname,"..","..", 'courseimage', 'uploads');
 
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
